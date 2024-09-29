5411c8dfced8db376c829a1214d17a42c0e37a79
эмир <114819359+tsechoev@users.noreply.github.com>
2024-09-29 15:29:12 +0300

    Create pz_5_1.py

---
+++ pz_5/pz_5_1.py
@@ -0,0 +1,8 @@
+#Составить функцию, которая выполнит суммирования числового ряда.
+def program(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
+numbers = (list(map(int,(input('Введите ряд чисел:')).split())))
+print(program(numbers))
